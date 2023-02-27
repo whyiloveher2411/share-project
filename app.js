d55e5afbb5e6cd3ef999fad7f6eb64f904358b50
ken <quandang@poccupine.com>
2023-02-27 10:58:38 +0700

    fix error app.js

--- app.js
+++ app.js
@@ -5,48 +5,6 @@ const app = express();
 const port = 3000;
 const folderName = 'static';
 
-
-
-app.get('/_prev-static', (req, res) => {
-
-
-
-    // check if folder exists
-    if (!fs.existsSync(folderName)) {
-        // create folder if it doesn't exist
-        fs.mkdirSync(folderName);
-    }
-
-    (async () => {
-
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://spacedev.vn/explore/loi-khuyen-danh-cho-nguoi-moi-bat-dau-lap-trinh-front-end', {
-            waitUntil: 'networkidle0',
-        });
-
-        // Wait for the HTML to fully render
-        await page.waitForSelector('html');
-
-        const tagAs = await page.evaluate(() => {
-            return document.querySelector('a');
-        });
-
-        console.log(tagAs);
-
-        const html = await page.content();
-
-        // Save the HTML to a file
-        fs.writeFile(folderName + '/example.html', html, function (err) {
-            if (err) throw err;
-            res.send('HTML saved to file!')
-        });
-
-        await browser.close();
-    })();
-
-})
-
 app.get(/.*/, (req, res) => {
 
     const url = req.url;
