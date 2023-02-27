4ae0c8d930addaf9fc8f3a394c816ace87f53a90
ken <quandang@poccupine.com>
2023-02-27 18:13:55 +0700

    test

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get(/.*/, async (req, res) => {
             res.send('HTML');
         }
     } else {
-        res.send('<meta http-equiv="Refresh" content="0; url=https://api.spacedev.vn' + url + ' />');
+        res.send('<h1>HTML</h1>');
     }
 })
 
